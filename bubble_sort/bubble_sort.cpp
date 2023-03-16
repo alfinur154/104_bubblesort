4e0f175ebce709b0d8fda93daaf782cfc60861bb
alfinur154 <alfinur27@gmail.com>
2023-03-16 15:42:36 +0700

    Memasukkan Variabel

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int a[20];
+int n;
 
-int a[20]:
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << end1;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		count << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}
