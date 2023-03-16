f58dcfa6ed016bddcea8892e34dd6562ce3dce0d
alfinur154 <alfinur27@gmail.com>
2023-03-16 18:03:50 +0700

    prosedur belum tersusun

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -36,3 +36,14 @@ void bubbleSortArray() {
 		}
 	}
 }
+
+void unsorted() {
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Elemen array belum tersusun" << endl;
+	cout << "===================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << endl;
+}
